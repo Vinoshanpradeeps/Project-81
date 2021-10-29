0a7477ec23134dbededc0810084fd2e6188b0f88
Vinoshanpradeeps <89023571+Vinoshanpradeeps@users.noreply.github.com>
2021-10-29 19:15:21 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,30 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+color = "black";
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(50, 100, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e){
+    color = document.getElementById("colour").value;
+    console.log(color);
+
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+    
+    console.log("x = "+ mouse_x+ ", y = "+ mouse_y);
+    circle(mouse_x,mouse_y);
+}
+
+function circle(mouse_x,mouse_y){
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 4;
+    ctx.arc(mouse_x, mouse_y, 40, 0, 2*Math.PI);
+    ctx.stroke();
+}
